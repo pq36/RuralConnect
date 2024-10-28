e3d1baa39f642b4118054e60a0982693dce40c19
Megasploit <109837700+pq36@users.noreply.github.com>
2024-10-28 15:09:34 +0530

    Update village.jsx

--- frontend/src/village.jsx
+++ frontend/src/village.jsx
@@ -1,7 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {dotenv} from 'dotenv'
-dotenv.config()
 function Village() {
   const { villageId } = useParams(); // Get villageId from URL
   const [villageData, setVillageData] = useState(null);
